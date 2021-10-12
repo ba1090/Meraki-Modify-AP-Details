502f1cc142945719c042f525c541f8f8f000286e
ba1090 <90853458+ba1090@users.noreply.github.com>
2021-10-12 16:20:12 +0200

    Update Meraki_Modify_AP_Details.py

--- Meraki_Modify_AP_Details.py
+++ Meraki_Modify_AP_Details.py
@@ -20,13 +20,13 @@ index = 0
 
 nameAP = input("+ Do you want to change NAMES? [y/n] ")
 tagAP = input("+ Do you want to change TAGS? [y/n] ")
+addressAP = input("+ Do you want to change ADDRESS? [y/n] ")
 
 now = datetime.now()
 start = now.strftime('%Y-%m-%d_%H-%M-%S')
 
 for i in xlsx_dict: # i prende il valore della riga da 0 a fine-1 di xlsx_dict
-
-    if nameAP=='y' and tagAP=='n':
+    if nameAP=='y':
         response = dashboard.devices.getDevice(
             i['serial']
         )
@@ -39,26 +39,20 @@ for i in xlsx_dict: # i prende il valore della riga da 0 a fine-1 di xlsx_dict
         else: 
             print('-',response['serial'], 'name',response['name'],'no change needed')
     
-    else:
+    if tagAP=='y':
         conftag = i['tags'].split(',')
-        if nameAP=='n' and tagAP=='y':
-            response = dashboard.devices.updateDevice(
+        response = dashboard.devices.updateDevice(
             i["serial"],
             tags = conftag
             )
-        else:
-            if nameAP=='y' and tagAP=='y':
-                response = dashboard.devices.getDevice(
-                    i['serial']
-                )
-                print('-',response['serial'], 'old name',response['name'])
-                response = dashboard.devices.updateDevice(
-                    i["serial"],
-                    name = i['hostname'],
-                    tags = conftag
-                )
+    
+    if addressAP=='y':
+        response = dashboard.devices.updateDevice(
+            i["serial"],
+            address = i["address"],
+            moveMapMarker = True
+            )
 
-    print('-',response['serial'], 'name',response['name'])
     index=index+1
     print('\n- Progress:',index,'of',len(xlsx_dict),'-',str(int((index/len(xlsx_dict))*100)),'%')
 
